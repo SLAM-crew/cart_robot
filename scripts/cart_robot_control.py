a3269ef04d0fc83bcbb2ff954fea49679ebb5ca0
sonieth <naganariselves@gmail.com>
2024-05-17 01:00:54 +0300

    add teleop into gazebo

--- scripts/cart_robot_control.py
+++ scripts/cart_robot_control.py
@@ -2,41 +2,49 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Float64
+from geometry_msgs.msg import Twist
 
 class CartRobotControl:
     def __init__(self):
+        rospy.loginfo("[Solution] started loaging")
+
         rospy.init_node('lidar_processing_node', anonymous=True)
         rospy.Subscriber("/cart_robot/lidar_main", LaserScan, self.lidar_callback)
+        self.cmd_vel = rospy.Publisher("/cmd_vel", Twist, queue_size=3)
+        self.joint_speed = 0.08  
 
-        self.up_right_wheel_controller = rospy.Publisher('/cart_robot/up_right_wheel_controller/command', Float64, queue_size=1)
-        self.up_left_wheel_controller = rospy.Publisher('/cart_robot/up_left_wheel_controller/command', Float64, queue_size=1)
-
-        self.down_right_wheel_controller = rospy.Publisher('/cart_robot/down_right_wheel_controller/command', Float64, queue_size=1)
-        self.down_left_wheel_controller = rospy.Publisher('/cart_robot/down_left_wheel_controller/command', Float64, queue_size=1)
-
+        rospy.loginfo("[Solution] loaded")
+    
+    # setted random values and directions in move_forward & turn_left & turn_right
+    # needed to be calibrated
+    # commented lidar_callback --> because debugging via teleop
     def move_forward(self):
-        joint_speed = 5.0  
-        self.up_right_wheel_controller.publish(-joint_speed)
-        self.up_left_wheel_controller.publish(-joint_speed)
-
-        self.down_right_wheel_controller.publish(-joint_speed)
-        self.down_left_wheel_controller.publish(-joint_speed)
+        self.command.linear.x = self.joint_speed
+        self.command.linear.y = 0.0
+        self.command.angular.z = 0.0
+        self.cmd_vel.publish(self.command)
 
     def turn_left(self):
-        joint_speed = 4.0  
-        self.up_right_wheel_controller.publish(-joint_speed)
-        self.down_right_wheel_controller.publish(-joint_speed)
-        self.up_left_wheel_controller.publish(joint_speed)
-        self.down_left_wheel_controller.publish(joint_speed)
+        self.command.linear.x = self.joint_speed
+        self.command.linear.y = 0.0
+        self.command.angular.z = 2.5
+        self.cmd_vel.publish(self.command)
+    
+    def turn_right(self):
+        self.command.linear.x = self.joint_speed
+        self.command.linear.y = 0.0
+        self.command.angular.z = -2.5
+        self.cmd_vel.publish(self.command)
 
     def lidar_callback(self, data):
-        distance_threshold = 4.0  
-        if data.ranges[len(data.ranges) // 2] < distance_threshold:
-            rospy.loginfo("Object detected in laser zone!")
-            self.turn_left()
-        else:
-            rospy.loginfo("No object detected in laser zone.")
-            self.move_forward()
+        pass
+        # distance_threshold = 4.0  
+        # if data.ranges[len(data.ranges) // 2] < distance_threshold:
+        #     rospy.loginfo("Object detected in laser zone!")
+        #     self.turn_left()
+        # else:
+        #     rospy.loginfo("No object detected in laser zone.")
+        #     self.move_forward()
             
 def main():
     try:
---
+++ scripts/move.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+import numpy as np
+from sensor_msgs.msg import Image
+import cv2
+from cv_bridge import CvBridge
+import rospy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64
+
+
+class SimpleController():
+
+    def __init__(self):
+        rospy.init_node('simple_controller', anonymous=True)
+        rospy.on_shutdown(self.shutdown)
+
+
+        self.up_right_wheel_controller = rospy.Publisher('/cart_robot/up_right_wheel_controller/command', Float64, queue_size=1)
+        self.up_left_wheel_controller = rospy.Publisher('/cart_robot/up_left_wheel_controller/command', Float64, queue_size=1)
+        self.down_right_wheel_controller = rospy.Publisher('/cart_robot/down_right_wheel_controller/command', Float64, queue_size=1)
+        self.down_left_wheel_controller = rospy.Publisher('/cart_robot/down_left_wheel_controller/command', Float64, queue_size=1)
+
+
+        self.cmd_subscriber = rospy.Subscriber("/cmd_vel", Twist, self.cmd_vel_callback)
+
+        self.rate = rospy.Rate(30)
+
+        self.v_x = 0
+        self.v_y = 0
+        self.w_z = 0
+        self.wheel_vel = np.zeros(4)
+
+    def cmd_vel_callback(self, msg:Twist):
+        rospy.loginfo("cmd vel command: {0} ".format(msg))
+
+        self.v_x = msg.linear.x
+        self.v_y = msg.linear.y
+        self.w_z = msg.angular.z
+
+    def spin(self):
+        while not rospy.is_shutdown():
+            # calculate the commands for wheels and publish
+
+            vw = np.array([self.v_x, self.v_y, self.w_z])
+            # wheel radius
+            r_w = 0.1016
+            # Base geometry:
+            l_x = 0.466725 / 2
+            l_y = 0.2667 / 2
+
+            # Inverse kinematic solution:
+            w = 1/r_w * np.array([
+                [1,  -1,  -(l_x + l_y)],
+                [1,   1,   (l_x + l_y)],
+                [1,   1,  -(l_x + l_y)],
+                [1,  -1,   (l_x + l_y)]
+            ])
+
+            # There's OUTPUT:
+            wheel_vel = w.dot(vw)
+
+            self.up_right_wheel_controller.publish(wheel_vel[0])
+            self.up_left_wheel_controller.publish(wheel_vel[1])
+            self.down_right_wheel_controller.publish(wheel_vel[2])
+            self.down_left_wheel_controller.publish(wheel_vel[3])
+
+            self.rate.sleep()
+
+    def shutdown(self):
+        self.up_right_wheel_controller.publish(0.0)
+        self.up_left_wheel_controller.publish(0.0)
+        self.down_right_wheel_controller.publish(0.0)
+        self.down_left_wheel_controller.publish(0.0)
+        rospy.sleep(1)
+
+
+simple_mover = SimpleController()
+simple_mover.spin()
